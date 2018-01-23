4852a4768a7436167c869ff261149db28f4a7451
James <bakerje@mac.com>
2018-01-23 15:47:53 +0000

    Commit for Sum of array test

--- array_recursion/spec/ArrayRecursionSpec.js
+++ array_recursion/spec/ArrayRecursionSpec.js
@@ -1,1 +1,23 @@
-ArrayRecursion.js
+describe("sumArray", function() {
+
+	it("1. should return an empty message when array is empty", function() {
+    array = [];
+    expect(sumArray(array)).toEqual('Array is empty');
+  });
+
+	it("2. should return the correct sum for an array with one or more element", function() {
+    array = [1];
+    expect(sumArray(array)).toEqual(1);
+  });
+
+	it("3. should return the correct sum for an array with one or more element", function() {
+    array = [1,2,3,4,5,6,7,8,9,10];
+    expect(sumArray(array)).toEqual(55);
+  });
+
+	it("4. should return the correct sum for an array with one or more element", function() {
+    array = [101,-2,15,-26,13,17,23];
+    expect(sumArray(array)).toEqual(141);
+  });
+
+});
--- array_recursion/src/ArrayRecursion.js
+++ array_recursion/src/ArrayRecursion.js
@@ -1,10 +1,13 @@
-var array_sum = function(my_array) {
-  if (my_array.length === 1) {
-    return my_array[0];
+var sumArray = function(array) {
+  if (array.length === 0) {
+	return "Array is empty";
+  }
+  if (array.length === 1) {
+    return array[0];
   }
   else {
-    return my_array.pop() + array_sum(my_array);
+    return array.pop() + sumArray(array);
   }
 };
 
-console.log(array_sum([1,2,3,4,5,6]));
+console.log(sumArray([101,-2,15,-26,13,17,23]));
