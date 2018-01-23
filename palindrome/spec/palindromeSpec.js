b2daa0c6542cfa56342ffbdfa9cd82f5feabbd6a
James <bakerje@mac.com>
2018-01-23 16:49:31 +0000

    Solved palindrome without recursion

--- palindrome/spec/palindromeSpec.js
+++ palindrome/spec/palindromeSpec.js
@@ -1,11 +1,21 @@
 describe('palindrome', function() {
 
-  it('should be able to identify a palindrome', function() {
+  it('should be able to identify if a word is a palindrome', function() {
     string = 'racecar';
-    expect(palindrome(string)).toEqual('rececar is a palindrome');
+    expect(palindrome(string)).toEqual('racecar is a palindrome');
   });
 
-  it('should be able to identify a palindrome', function() {
+  it('should be able to identify if a number is a palindrome', function() {
+    string = '1234321';
+    expect(palindrome(string)).toEqual('1234321 is a palindrome');
+  });
+
+  it('should be able to identify if a word is not a palindrome', function() {
+    string = 'test';
+    expect(palindrome(string)).toEqual('test is not a palindrome');
+  });
+
+  it('should be able to identify if a number is not a palindrome', function() {
     string = 'test';
     expect(palindrome(string)).toEqual('test is not a palindrome');
   });
--- palindrome/src/palindrome.js
+++ palindrome/src/palindrome.js
@@ -1,6 +1,9 @@
-var palindrome = function(string) {
-
-
-
-
-};
+function palindrome(string) {
+	string = string.toString();
+	if ((string == string.split('').reverse().join('')) === true) {
+	return ''+ string +' is a palindrome';
+    }
+	else {
+	return ''+ string +' is not a palindrome';
+    }
+}
