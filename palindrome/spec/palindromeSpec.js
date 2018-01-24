1df6bdc2016d3356ae99680473cf0b89dd7814f4
James <bakerje@mac.com>
2018-01-24 11:04:24 +0000

    added further test cases and detail

--- palindrome/spec/palindromeSpec.js
+++ palindrome/spec/palindromeSpec.js
@@ -1,22 +1,42 @@
-describe('palindrome', function() {
+describe('isPalindrome', function() {
 
-  it('should be able to identify if a word is a palindrome', function() {
+  it('1. should be able to identify if the string is empty', function() {
+    string = '';
+    expect(isPalindrome(string)).toEqual('string is empty');
+  });
+
+  it('2. should be able to identify if the string has only one character', function() {
+    string = 'a';
+    expect(isPalindrome(string)).toEqual('a is a palindrome');
+  });
+
+  it('3. should be able to identify if a word is a palindrome', function() {
     string = 'racecar';
-    expect(palindrome(string)).toEqual(true);
+    expect(isPalindrome(string)).toEqual('racecar is a palindrome');
   });
 
-  it('should be able to identify if a number is a palindrome', function() {
+  it('4. should be able to identify if a number is a palindrome', function() {
     string = '1234321';
-    expect(palindrome(string)).toEqual(true);
+    expect(isPalindrome(string)).toEqual('1234321 is a palindrome');
   });
 
-  it('should be able to identify if a word is not a palindrome', function() {
-    string = 'test';
-    expect(palindrome(string)).toEqual(false);
+  it('5. should be able to identify if an alphanumeric is a palindrome', function() {
+    string = '123abba321';
+    expect(isPalindrome(string)).toEqual('123abba321 is a palindrome');
   });
 
-  it('should be able to identify if a number is not a palindrome', function() {
+  it('6. should be able to identify if a word is not a palindrome', function() {
     string = 'test';
-    expect(palindrome(string)).toEqual(false);
+    expect(isPalindrome(string)).toEqual('test is not a palindrome');
+  });
+
+  it('7. should be able to identify if a number is not a palindrome', function() {
+    string = '123456';
+    expect(isPalindrome(string)).toEqual('123456 is not a palindrome');
+  });
+
+  it('8. should be able to identify if an alphanumeric is not a palindrome', function() {
+    string = '123abcd456';
+    expect(isPalindrome(string)).toEqual('123abcd456 is not a palindrome');
   });
 });
--- palindrome/src/palindrome.js
+++ palindrome/src/palindrome.js
@@ -1,5 +1,11 @@
 function palindrome(string) {
-	if (string.length === 0) { return true; }
+	if (string.length === 0) { return 'string is empty'; }
 	if (string[0] !== string[string.length-1]) { return false;}
-	return palindrome(string.slice(1, string.length-1));	
+	return palindrome(string.slice(1, string.length-1));
+}
+
+function isPalindrome(string) {
+	if (string.length === 0) { return 'string is empty'; }
+	else if (palindrome(string)) { return ''+string+' is a palindrome';}
+	else { return ''+string+' is not a palindrome';}
 }
